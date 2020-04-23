8cd59ee29990d50f1fac03cc5dfdce8227dcf372
Julliana Bauer <bjulliana@gmx.com>
2020-04-23 00:21:50 -0400

    import global scss, add logo and helper file

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `@import "@/assets/scss/main.scss";`,
-      },
-    },
-  },
+  // css: {
+  //   loaderOptions: {
+  //     scss: {
+  //       prependData: `@import "@/assets/scss/main.scss";`,
+  //     },
+  //   },
+  // },
 };
