36dc2dd02bb8f4f0f434cea57fcc0e9e20276408
Julliana Bauer <bjulliana@gmx.com>
2020-04-24 19:40:00 -0400

    resolve alias and format files

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
+const path = require('path');
+
 module.exports = {
-  // css: {
-  //   loaderOptions: {
-  //     scss: {
-  //       prependData: `@import "@/assets/scss/main.scss";`,
-  //     },
-  //   },
-  // },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "@/assets/scss/global.scss";`,
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, './src'),
+      },
+    },
+  },
 };
