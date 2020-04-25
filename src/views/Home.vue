a9b511a702e86e23616768e91c3f45d687698348
Julliana Bauer <bjulliana@gmx.com>
2020-04-25 16:48:08 -0400

    content image component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Hero />
+    <ContentImage />
   </div>
 </template>
 
 <script>
 import Hero from '@/components/Hero.vue';
+import ContentImage from '@/components/ContentImage.vue';
 
 export default {
   name: 'home',
   components: {
     Hero,
+    ContentImage,
   },
 };
 </script>
