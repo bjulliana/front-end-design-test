ee8914cf9cae96acb1aae12eb9f44069977b7b22
Julliana Bauer <bjulliana@gmx.com>
2020-04-26 17:40:28 -0400

    fix scroll animation directive

--- src/directives/scrollAnimation.js
+++ src/directives/scrollAnimation.js
@@ -1,6 +1,8 @@
 const animatedScrollObserver = new IntersectionObserver((entries, animatedScrollObserver) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
+      entry.target.classList.add('enter');
+      entry.target.className.replace('before-enter', 'enter');
       animatedScrollObserver.unobserve(entry.target);
     }
   });
@@ -8,10 +10,7 @@ const animatedScrollObserver = new IntersectionObserver((entries, animatedScroll
 
 export default {
   bind(el, binding) {
-    el.classList.add('before-enter');
-    if (binding.arg) {
-      el.classList.add(`before-enter__${binding.arg}`);
-    }
+    el.classList.add(`before-enter__${binding.arg}`);
     animatedScrollObserver.observe(el);
   },
 };
