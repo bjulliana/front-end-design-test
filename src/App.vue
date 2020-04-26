784ee63aa4ee287bcfaa910944b34957ab717733
Julliana Bauer <bjulliana@gmx.com>
2020-04-26 17:52:08 -0400

    footer component file and add to template

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <Header msg="Welcome to Your Vue.js App" />
-    </div>
+    <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer';
 
 export default {
   name: 'home',
   components: {
+    Footer,
     Header,
   },
 };
 </script>
-
-<style lang="scss"></style>
